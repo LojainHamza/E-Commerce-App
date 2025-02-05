a9c04211344dfad8d06a71ec13f8eccf3c76c740
LojainHamza <lojainehab17@gmail.com>
2025-02-06 00:11:46 +0200

    empty flutter application.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:e_commerce_app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen()
+      },
+    );
+  }
+}
