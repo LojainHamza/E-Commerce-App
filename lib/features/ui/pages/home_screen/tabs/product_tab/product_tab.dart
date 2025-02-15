3b97492584ec60255c934d0b044b3f2fd882774c
LojainHamza <lojainehab17@gmail.com>
2025-02-15 02:09:16 +0200

    finished category tab ui and models.

--- lib/features/ui/pages/home_screen/tabs/product_tab/product_tab.dart
+++ lib/features/ui/pages/home_screen/tabs/product_tab/product_tab.dart
@@ -1,10 +1,36 @@
+import 'package:e_commerce_app/core/utils/app_routes.dart';
+import 'package:e_commerce_app/features/ui/widgets/product_tab_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ProductTab extends StatelessWidget {
   const ProductTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+            child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 2 / 2.6.h,
+                    crossAxisSpacing: 16.w,
+                    mainAxisSpacing: 16.h),
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      //TODO: navigate to product details screen
+                      Navigator.pushNamed(context, AppRoutes.productRoute);
+                    },
+                    child: ProductTabItem(),
+                  );
+                })),
+      ],
+    ));
   }
 }
---
+++ lib/features/ui/widgets/product_tab_item.dart
@@ -0,0 +1,107 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_commerce_app/core/utils/app_colors.dart';
+import 'package:e_commerce_app/core/utils/app_styles.dart';
+import 'package:e_commerce_app/features/ui/widgets/custom_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ProductTabItem extends StatelessWidget {
+  const ProductTabItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.r),
+          border: Border.all(
+              color: AppColors.primaryColor.withAlpha(200), width: 2)),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(15.r),
+                child: CachedNetworkImage(
+                  width: 190.w,
+                  height: 120.h,
+                  fit: BoxFit.cover,
+                  imageUrl:
+                      "https://redtape.com/cdn/shop/files/RSO4102_1_c0a68240-77e5-4003-8465-bff2bb2280a7.jpg?v=1738392818",
+                  placeholder: (context, url) => Center(
+                      child: CircularProgressIndicator(
+                          color: AppColors.primaryColor)),
+                  errorWidget: (context, url, error) =>
+                      Icon(Icons.error, color: AppColors.redColor),
+                ),
+              ),
+              Positioned(
+                  top: 8.h,
+                  right: 8.w,
+                  child: CircleAvatar(
+                    backgroundColor: AppColors.whiteColor,
+                    radius: 20.r,
+                    child: Center(
+                      child: IconButton(
+                        color: AppColors.primaryColor,
+                        padding: EdgeInsets.zero,
+                        iconSize: 30.r,
+                        onPressed: () {
+                          //TODO: add to favorites
+                        },
+                        icon: Icon(
+                          Icons.favorite_border,
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                    ),
+                  )),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomText(
+                    text: 'Nike Air Jordan',
+                    textStyle: AppStyles.regular14darkBlue),
+                SizedBox(height: 2.h),
+                Row(
+                  children: [
+                    CustomText(
+                        text: 'EGP 2000',
+                        textStyle: AppStyles.regular14darkBlue),
+                    SizedBox(width: 8.w),
+                    CustomText(
+                        text: 'EGP 1500',
+                        textStyle: AppStyles.regular11Grey
+                            .copyWith(decoration: TextDecoration.lineThrough))
+                  ],
+                ),
+                SizedBox(height: 2.h),
+                Row(
+                  children: [
+                    CustomText(
+                        text: 'Review (4.6)',
+                        textStyle: AppStyles.regular12darkBlue),
+                    Icon(Icons.star, color: AppColors.yellowColor, size: 25.sp),
+                    Spacer(flex: 1),
+                    InkWell(
+                      splashColor: Colors.transparent,
+                      onTap: () {
+                        // TODO: add to cart
+                      },
+                      child: Icon(Icons.add_circle,
+                          color: AppColors.primaryColor, size: 32.sp),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
