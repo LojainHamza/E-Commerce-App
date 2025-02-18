efd95e4d159575ea1ceefbb3e00b817bd8849e0d
LojainHamza <lojainehab17@gmail.com>
2025-02-18 17:48:28 +0200

    cart screen app bar done.

--- lib/features/ui/pages/cart_screen/cart_screen.dart
+++ lib/features/ui/pages/cart_screen/cart_screen.dart
@@ -1,10 +1,31 @@
+import 'package:e_commerce_app/core/utils/app_colors.dart';
+import 'package:e_commerce_app/core/utils/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: AppColors.primaryColor),
+        title: Text('Cart', style: AppStyles.medium20darkBlue),
+        centerTitle: true,
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(Icons.search, color: AppColors.primaryColor, size: 30.sp),
+              SizedBox(width: 10.w),
+              Icon(Icons.shopping_cart,
+                  color: AppColors.primaryColor, size: 30.sp),
+              SizedBox(width: 10.w),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
