beb3d07b1f7d22675596e013e1a4026e753aff6b
LojainHamza <lojainehab17@gmail.com>
2025-02-21 22:03:21 +0200

    favorite tab ui done.

--- lib/features/ui/pages/home_screen/tabs/favorite_tab/favorite_tab.dart
+++ lib/features/ui/pages/home_screen/tabs/favorite_tab/favorite_tab.dart
@@ -1,10 +1,161 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_commerce_app/core/utils/app_colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class FavoriteTab extends StatelessWidget {
   const FavoriteTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 12.h),
+        child: ListView(
+          children: [
+            _buildWishlistItem(
+              imageUrl:
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk8_hkV77-7nuIywXiWpuOoic6HvUtxv7HSg&s',
+              title: 'Nike Air Jordon',
+              price: 1200,
+              originalPrice: 1500,
+            ),
+            SizedBox(height: 16.h),
+            _buildWishlistItem(
+              imageUrl:
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk8_hkV77-7nuIywXiWpuOoic6HvUtxv7HSg&s',
+              title: 'Tall Cotton Dress',
+              price: 600,
+              originalPrice: 750,
+            ),
+            SizedBox(height: 16.h),
+            _buildWishlistItem(
+              imageUrl:
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk8_hkV77-7nuIywXiWpuOoic6HvUtxv7HSg&s',
+              title: 'GUESS Women\'s',
+              price: 1200,
+              originalPrice: 1500,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWishlistItem({
+    required String imageUrl,
+    required String title,
+    required double price,
+    required double originalPrice,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.r),
+        border: Border.all(color: Colors.grey, width: 2),
+      ),
+      child: Row(
+        children: [
+          CachedNetworkImage(
+            width: 135.w,
+            height: 145.h,
+            fit: BoxFit.cover,
+            imageUrl: imageUrl,
+            placeholder: (context, url) => Center(
+              child: CircularProgressIndicator(),
+            ),
+            errorWidget: (context, url, error) =>
+                Icon(Icons.error, color: Colors.red),
+          ),
+          SizedBox(width: 16.w),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.primaryColor,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Icon(CupertinoIcons.delete),
+                    SizedBox(width: 5.w)
+                  ],
+                ),
+                SizedBox(height: 8.h),
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.black,
+                      radius: 10.r,
+                    ),
+                    SizedBox(width: 10.w),
+                    Text(
+                      'Black Color | Size: 40',
+                      style: TextStyle(
+                        fontSize: 18.sp,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 15.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'EGP $price',
+                          style: TextStyle(
+                            fontSize: 18.sp,
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.primaryColor,
+                          ),
+                        ),
+                        SizedBox(height: 4.h),
+                        Text(
+                          'EGP $originalPrice',
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            color: AppColors.greyColor,
+                            decoration: TextDecoration.lineThrough,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 2.w),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.primaryColor,
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'Add To Cart',
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
